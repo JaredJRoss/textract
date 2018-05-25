69fe5fdcf9100a92edf2ba5287c15ab894ef2561
Jared Ross <jaredrossj@gmail.com>
2018-05-24 22:35:39 -0400

    removed root

--- setup.py
+++ setup.py
@@ -53,8 +53,8 @@ setup(
     license='MIT',
     scripts=scripts,
     packages=[
-        '/root/textract/textract',
-        '/root/textract/textract.parsers',
+        'textract/textract',
+        'textract/textract.parsers',
     ],
     install_requires=dependencies,
     dependency_links=dependency_links,
